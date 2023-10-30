c51c6e9ef0bf60fd02dd738b5b99e8140ef9da2c
Kylin <liux1118@qq.com>
2023-10-30 17:08:54 +0800

    feat: 添加axios、router、pinia

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,15 @@ import ArcoVue from '@arco-design/web-vue'
 import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 import '@arco-design/web-vue/dist/arco.css'
 
+import router from './router'
+import store from './store'
+
 const app = createApp(App)
+
 app.use(ArcoVue)
 app.use(ArcoVueIcon)
+
+app.use(router)
+app.use(store)
+
 app.mount('#app')
